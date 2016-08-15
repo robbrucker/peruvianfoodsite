bd5648d55af5683bc53cf536e627f282b75abb07
Robert Brucker <brucker.rob@gmail.com>
2016-08-14 23:31:33 -0400

    ignore these files

--- config/deploy.rb
+++
@@ -1,9 +0,0 @@
-# config valid only for current version of Capistrano
-lock '3.5.0'
-
-set :application, 'peruvian_food_in_rva'
-set :repo_url, 'https://github.com/robbrucker/peruvianfoodsite.git'
-set :git_https_username, 'yourusername'
-set :git_https_password, 'yourpassword'
-set :deploy_to, 'home/peruvian'
-set :scm, :git
--- config/deploy/production.rb
+++
@@ -1,9 +0,0 @@
-
-# production.rb
-set :application, 'peruvian_food_in_rva'
-
-## Servers we are going to deploy to ----------------------
-server 'xxx.xxx.xxx.xxx', user: 'youruser'
-
-## Server Settings ----------------------------------------
-set :deploy_to, "/home/peruvian"
